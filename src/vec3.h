490664175caca6886f22c0179889b8767fe69eac
Michal Andrasiak <michal.andrasiak@gmail.com>
2022-04-08 23:00:19 +0200

    Make vector, color and point different types

--- src/vec3.h
+++ src/vec3.h
@@ -2,11 +2,18 @@
 #define VECTOR_3
 
 #include <array>
+enum class type { point, vector, color };
 
-template <typename T> class vec3 {
+template <type Type, typename T> class vec3 {
 public:
   constexpr vec3() = default;
   constexpr vec3(T a, T b, T c) : d_{a, b, c} {}
+
+  constexpr vec3(const vec3 &src) = default;
+  constexpr vec3(vec3 &&src) = default;
+
+  constexpr vec3 &operator=(const vec3 &src) = default;
+  constexpr vec3 &operator=(vec3 &&src) = default;
 
   constexpr T &r() noexcept { return d_[0]; }
   constexpr T &g() noexcept { return d_[1]; }
@@ -73,60 +80,79 @@ private:
   std::array<T, 3> d_;
 };
 
-template <typename T>
-std::ostream &operator<<(std::ostream &out, const vec3<T> &v) {
+template <type Type, typename T>
+std::ostream &operator<<(std::ostream &out, const vec3<Type, T> &v) {
   return out << v.get<0>() << ' ' << v.get<1>() << ' ' << v.get<2>();
 }
 
-template <typename T>
-constexpr vec3<T> operator+(const vec3<T> &rhs, const vec3<T> &lhs) {
-  return vec3<T>{rhs.get<0>() + lhs.get<0>(), rhs.get<1>() + lhs.get<1>(),
-                 rhs.get<2>() + lhs.get<2>()};
+template <type Type, typename T>
+constexpr vec3<Type, T> operator+(const vec3<Type, T> &rhs,
+                                  const vec3<Type, T> &lhs) {
+  return vec3<Type, T>{rhs.get<0>() + lhs.get<0>(), rhs.get<1>() + lhs.get<1>(),
+                       rhs.get<2>() + lhs.get<2>()};
 }
 
-template <typename T>
-constexpr vec3<T> operator-(const vec3<T> &rhs, const vec3<T> &lhs) {
-  return vec3<T>{rhs.get<0>() - lhs.get<0>(), rhs.get<1>() - lhs.get<1>(),
-                 rhs.get<2>() - lhs.get<2>()};
+template <type Type, typename T>
+constexpr vec3<Type, T> operator-(const vec3<Type, T> &rhs,
+                                  const vec3<Type, T> &lhs) {
+  return vec3<Type, T>{rhs.get<0>() - lhs.get<0>(), rhs.get<1>() - lhs.get<1>(),
+                       rhs.get<2>() - lhs.get<2>()};
 }
 
-template <typename T>
-constexpr vec3<T> operator*(const vec3<T> &rhs, const vec3<T> &lhs) {
-  return vec3<T>{rhs.get<0>() * lhs.get<0>(), rhs.get<1>() * lhs.get<1>(),
-                 rhs.get<2>() * lhs.get<2>()};
+template <type Type, typename T>
+constexpr vec3<Type, T> operator*(const vec3<Type, T> &rhs,
+                                  const vec3<Type, T> &lhs) {
+  return vec3<Type, T>{rhs.get<0>() * lhs.get<0>(), rhs.get<1>() * lhs.get<1>(),
+                       rhs.get<2>() * lhs.get<2>()};
 }
 
-template <typename T> constexpr vec3<T> operator*(T rhs, const vec3<T> &lhs) {
-  return vec3{rhs * lhs.get<0>(), rhs * lhs.get<1>(), rhs * lhs.get<2>()};
+template <type Type, typename T>
+constexpr vec3<Type, T> operator*(T rhs, const vec3<Type, T> &lhs) {
+  return vec3<Type, T>{rhs * lhs.get<0>(), rhs * lhs.get<1>(),
+                       rhs * lhs.get<2>()};
 }
 
-template <typename T>
-constexpr vec3<T> operator*(const vec3<T> &rhs, double lhs) {
-  return vec3<T>{rhs.get<0>() * lhs, rhs.get<1>() * lhs, rhs.get<2>() * lhs};
+template <type Type, typename T>
+constexpr vec3<Type, T> operator*(const vec3<Type, T> &rhs, double lhs) {
+  return vec3<Type, T>{rhs.get<0>() * lhs, rhs.get<1>() * lhs,
+                       rhs.get<2>() * lhs};
 }
 
-template <typename T> constexpr vec3<T> operator/(const vec3<T> &lhs, T rhs) {
+template <type Type, typename T>
+constexpr vec3<Type, T> operator/(const vec3<Type, T> &lhs, T rhs) {
   return (1 / rhs) * lhs;
 }
 
-template <typename T> constexpr T dot(const vec3<T> &rhs, const vec3<T> &lhs) {
+template <type Type, typename T>
+constexpr T dot(const vec3<Type, T> &rhs, const vec3<Type, T> &lhs) {
   return rhs.get<0>() * lhs.get<0>() + rhs.get<1>() * lhs.get<1>() +
          rhs.get<2>() * lhs.get<2>();
 }
 
-template <typename T>
-constexpr vec3<T> cross(const vec3<T> &lhs, const vec3<T> &rhs) {
-  return vec3<T>{lhs.get<1>() * rhs.get<2>() - lhs.get<2>() * rhs.get<1>(),
-                 lhs.get<2>() * rhs.get<0>() - lhs.get<0>() * rhs.get<2>(),
-                 lhs.get<0>() * rhs.get<1>() - lhs.get<1>() * rhs.get<0>()};
+template <type Type, typename T>
+constexpr vec3<Type, T> cross(const vec3<Type, T> &lhs,
+                              const vec3<Type, T> &rhs) {
+  return vec3<Type, T>{
+      lhs.get<1>() * rhs.get<2>() - lhs.get<2>() * rhs.get<1>(),
+      lhs.get<2>() * rhs.get<0>() - lhs.get<0>() * rhs.get<2>(),
+      lhs.get<0>() * rhs.get<1>() - lhs.get<1>() * rhs.get<0>()};
 }
 
-template <typename T> constexpr vec3<T> unit_vector(const vec3<T> &v) {
+template <type Type, typename T>
+constexpr vec3<Type, T> unit_vector(const vec3<Type, T> &v) {
   return v / v.length();
 }
 
-using vec3f = vec3<float>;
-using vec3d = vec3<double>;
-using vec3i = vec3<int>;
+using vec3f = vec3<type::vector, float>;
+using vec3d = vec3<type::vector, double>;
+using vec3i = vec3<type::vector, int>;
+
+using point3f = vec3<type::point, float>;
+using point3d = vec3<type::point, double>;
+using point3i = vec3<type::point, int>;
+
+using color3f = vec3<type::color, float>;
+using color3d = vec3<type::color, double>;
+using color3i = vec3<type::color, int>;
 
 #endif
