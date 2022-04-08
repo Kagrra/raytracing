bd6bd9341d36480f231fc48de4778ec13a02ed26
Michal Andrasiak <michal.andrasiak@gmail.com>
2022-04-08 22:15:03 +0200

    Add vec3 free functions

--- src/vec3.h
+++ src/vec3.h
@@ -73,6 +73,58 @@ private:
   std::array<T, 3> d_;
 };
 
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const vec3<T> &v) {
+  return out << v.get<0>() << ' ' << v.get<1>() << ' ' << v.get<2>();
+}
+
+template <typename T>
+constexpr vec3<T> operator+(const vec3<T> &rhs, const vec3<T> &lhs) {
+  return vec3<T>{rhs.get<0>() + lhs.get<0>(), rhs.get<1>() + lhs.get<1>(),
+                 rhs.get<2>() + lhs.get<2>()};
+}
+
+template <typename T>
+constexpr vec3<T> operator-(const vec3<T> &rhs, const vec3<T> &lhs) {
+  return vec3<T>{rhs.get<0>() - lhs.get<0>(), rhs.get<1>() - lhs.get<1>(),
+                 rhs.get<2>() - lhs.get<2>()};
+}
+
+template <typename T>
+constexpr vec3<T> operator*(const vec3<T> &rhs, const vec3<T> &lhs) {
+  return vec3<T>{rhs.get<0>() * lhs.get<0>(), rhs.get<1>() * lhs.get<1>(),
+                 rhs.get<2>() * lhs.get<2>()};
+}
+
+template <typename T> constexpr vec3<T> operator*(T rhs, const vec3<T> &lhs) {
+  return vec3{rhs * lhs.get<0>(), rhs * lhs.get<1>(), rhs * lhs.get<2>()};
+}
+
+template <typename T>
+constexpr vec3<T> operator*(const vec3<T> &rhs, double lhs) {
+  return vec3<T>{rhs.get<0>() * lhs, rhs.get<1>() * lhs, rhs.get<2>() * lhs};
+}
+
+template <typename T> constexpr vec3<T> operator/(const vec3<T> &lhs, T rhs) {
+  return (1 / rhs) * lhs;
+}
+
+template <typename T> constexpr T dot(const vec3<T> &rhs, const vec3<T> &lhs) {
+  return rhs.get<0>() * lhs.get<0>() + rhs.get<1>() * lhs.get<1>() +
+         rhs.get<2>() * lhs.get<2>();
+}
+
+template <typename T>
+constexpr vec3<T> cross(const vec3<T> &lhs, const vec3<T> &rhs) {
+  return vec3<T>{lhs.get<1>() * rhs.get<2>() - lhs.get<2>() * rhs.get<1>(),
+                 lhs.get<2>() * rhs.get<0>() - lhs.get<0>() * rhs.get<2>(),
+                 lhs.get<0>() * rhs.get<1>() - lhs.get<1>() * rhs.get<0>()};
+}
+
+template <typename T> constexpr vec3<T> unit_vector(const vec3<T> &v) {
+  return v / v.length();
+}
+
 using vec3f = vec3<float>;
 using vec3d = vec3<double>;
 using vec3i = vec3<int>;
